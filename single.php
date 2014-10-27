544aa80ad9b8418d39505cbe94ec367b28819447
Ashlynn Pai <ashlynnpai@gmail.com>
2014-10-27 10:37:18 -0400

    sixth commit

--- single.php
+++ single.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">   
   <div class="blog-header"></div>
       <div class="row">     
-         <div class="col-md-9">
+         <div class="col-md-9" id="single-blog">
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <div class="page-header">      
