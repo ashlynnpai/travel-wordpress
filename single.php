aa0670be9c8d2a22b3fc6270be7c2915c2d26328
Ashlynn Pai <ashlynnpai@gmail.com>
2015-05-07 20:49:43 -0400

    modify style single

--- single.php
+++ single.php
@@ -1,44 +1,48 @@
 <?php get_header(); ?>
 
 <div class="container-fluid">   
-  <div class="blog-header"></div>
-      <div class="row">     
-         <div class="col-md-9" id="single-blog">
+  <div class="row">     
+    <div class="col-md-9" id="single-blog">
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <div class="page-header">      
-
-    <h1><?php the_title(); ?></h1>
-            <p>
-              By <?php the_author(); ?> 
-              on <?php echo the_time('l, F jS, Y');?>
-              in <?php the_category( ', ' ); ?>.
-              <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
-            </p>
-            </div>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="page-header">  
+        <h1><?php the_title(); ?></h1>
+        <p>
+          By <?php the_author(); ?> 
+          on <?php echo the_time('l, F jS, Y');?>
+          in <?php the_category( ', ' ); ?>.
+          <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
+        </p>
+      </div>
       
-          <p><?php the_content(); ?></p>
-          <p><?php the_tags(); ?></p>          
-                
-    <div class="next-posts"><?php next_post_link('%link', 'Newer entry &gt;') ?></div>
-    <div class="prev-posts"><?php previous_post_link('%link', '&lt; Older entry') ?></div>
+      <p><?php the_content(); ?></p>
 
-		<?php comments_template( '', true ); ?> 
-        
-           <?php endwhile; else: ?>
-          
-          <div class="page-header">
-            <h1>Oh no!</h1>
-          </div>
+      <p id="vertical-space"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/tag.png" &nbsp; >  <?php the_tags(); ?></p>     
 
-          <p>No content is appearing for this page!</p>
+      <div class="prev-posts" id="vertical-space"><?php previous_post_link('%link','<p><img src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/left.png" /> &nbsp; Previous Post</p>'); ?></div>
 
-        <?php endif; ?>
-     
-        </div>
-       
-      <?php get_sidebar( 'blog' ); ?>
- </div>
+      <div class="next-posts" id="vertical-space"><?php next_post_link('%link','<p>Next Post &nbsp; <img src="' . get_bloginfo( 'stylesheet_directory' ) . '/img/right.png" /></p>'); ?></div>        
+
+
+      <?php comments_template( '', true ); ?>       
+
+    <?php endwhile; else: ?>
+
+      <div class="page-header">
+      <h1>Oh no!</h1>
+      </div>
+
+      <p>No content is appearing for this page!</p>
+
+    <?php endif; ?>
+
+    </div>
+
+  <?php get_sidebar( 'blog' ); ?>
+  </div>
 </div>
 
 <?php get_footer(); ?>
+
+
+
