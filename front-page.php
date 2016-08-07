c4a216b5d8f889915e62236ff65b644c4c5ff44a
ashlynnpai <ashlynnpai@gmail.com>
2016-08-07 16:06:23 -0400

    adjust front page layout

--- front-page.php
+++ front-page.php
@@ -29,25 +29,25 @@
         </div>
     </div>   
           
-    <div class="row" id="row-two">
-          
-        <div class="col-md-4">
-            <h2><img class="icon" src="wp-content/themes/travel/img/people.png"/></h2>
-    <p>Experience the local community through work, study and volunteering.  Opportunities are available ranging from a few hours to years.</p>
-        </div>  
-          
-        <div class="col-md-4">
-            <h2><img class="icon" src="wp-content/themes/travel/img/tunnel.png"/></h2>
-    <p>Make long distance journeys by train.  Hike or bike to the next city ... or country.  See more of the country by trying out different means of travel. </p>
-        </div>    
-          
-        <div class="col-md-4">
-            <h2><img class="icon" src="wp-content/themes/travel/img/dollar.png"/></h2>
-    <p>Save money without sacrificing the quality of your experience.  Try the street food or rent an apartment from a local. </p>
-        </div>      
-               
+    <div class="row" id="row-two">      
+        <div class="col-md-10 col-md-offset-1 text-center">        
+            <div class="col-md-4 icon-info">
+                <h2><img src="wp-content/themes/travel/img/signs.png"/></h2>
+        <p>Experience the local community as more than a tourist through work, study and volunteering. Make your vacation more than a vacation.</p>
+            </div>  
+              
+            <div class="col-md-4">
+                <h2><img src="wp-content/themes/travel/img/bus.png"/></h2>
+        <p>Travel the slow way. Go by bus, car, train, boat, or bike. Walk, hike or ride a horse or camel to the next town or the next country. Some of the best parts are missed by flying.</p>
+            </div>    
+              
+            <div class="col-md-4">
+                <h2><img src="wp-content/themes/travel/img/food.png"/></h2>
+        <p>Save money without sacrificing the quality of your experience.  Try the street food or the local produce and rent an apartment from a local, or do a homestay or farmstay.</p>
+            </div>      
+        </div>           
     </div>      
    
-
+<!-- Free icons by Round Icons distributed by tympanus.net at http://tympanus.net/codrops/2014/10/17/freebie-travel-ecology-icon-set/ -->
 
 <?php get_footer(); ?>
