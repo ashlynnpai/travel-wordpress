d2b3175bfeb58728673ffbf9c05440ba044d780c
ashlynnpai <ashlynnpai@gmail.com>
2016-09-03 22:09:16 -0400

    adjust sidebar and search box styling

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div>
+        <input type="text" value="" placeholder="Search" name="s" id="s">
+        <span><i class="glyphicon glyphicon-search"></i></span> 
+    </div>
+</form>
