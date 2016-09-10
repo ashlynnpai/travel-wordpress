0ff6eb8b45187486383def01a12ce5bf42f1aa2b
ashlynnpai <ashlynnpai@gmail.com>
2016-09-10 16:06:16 -0400

    style search box

--- searchform.php
+++ searchform.php
@@ -1,6 +1,7 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div>
         <input type="text" value="" placeholder="Search" name="s" id="s">
-        <span><i class="glyphicon glyphicon-search"></i></span> 
+        <button class="btn" id="search-button"><span class="glyphicon glyphicon-search" type="submit" id="searchsubmit"></span></button>
+
     </div>
 </form>
