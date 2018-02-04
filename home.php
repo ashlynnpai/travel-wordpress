3aa8de9c49338f28238f8b53fd66b2bcd2caaa65
Ashlynn Pai <ashlynnpai@gmail.com>
2018-02-04 21:10:38 +0000

    update home.php for pagination

--- home.php
+++ home.php
@@ -1,47 +1,31 @@
 <?php get_header(); ?>
-
   <div class="container-fluid">   
     <div class="row" id="blog-list">
- 
       <div class="col-md-9">
-
         <div class="page-header">
           <h1 id="blog-head"><?php wp_title(''); ?></h1>
-        </div>
-
-      <?php query_posts('showposts=10'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-          <article class="post">
-            
-            <h3 id="blog-listing-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>             </h3>
+        </div> 
+        <?php 
+          $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+          $args = array('posts_per_page' => 5, 'paged' => $paged );
+          query_posts($args); ?>
+          <?php if ( have_posts() ) : while (have_posts()) : the_post(); ?>
+          	<article class="post">
+            <h3 id="blog-listing-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             <p>
-              By <?php the_author(); ?> 
               on <?php echo the_time('l, F jS, Y');?>
-              in <?php the_category( ', ' ); ?>.
-              <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
             </p>
-
             <hr>
-            
             <?php the_excerpt(); ?>
-
           </article>
-
-         
-        <?php endwhile; else: ?>
-          
-          <div class="page-header">
-            <h1>Oh no!</h1>
-          </div>
-
-          <p>No content is appearing for this page!</p>
-
-        <?php endif; wp_reset_query(); ?>
-
-          </div>
-      
+          <?php endwhile; ?>
+          <?php next_posts_link(); ?>
+          <?php previous_posts_link(); ?>
+          <?php else : ?>
+          <?php endif; ?>
+        </div>
       <?php get_sidebar( 'blog' ); ?>
-
     </div>
-</div>
+  </div>
 <?php get_footer(); ?>
+
